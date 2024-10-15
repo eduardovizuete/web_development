4609f81ed97ccb054f5060403beb4ccb2fdf237f
eduvizuete <vizuete.eduardo@gmail.com>
2024-10-15 12:47:03 -0500

    create a middleware and function to handle possible errors

--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -6,8 +6,6 @@ import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
-app.use(express.json());
-
 // initialize dotenv
 dotenv.config();
 
@@ -24,5 +22,17 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
+app.use(express.json());
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
