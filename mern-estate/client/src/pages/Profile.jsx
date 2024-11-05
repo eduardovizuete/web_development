544514fae1ad192e38caa161fbe80c016edade0a
edu <vizuete.eduardo@gmail.com>
2024-11-05 18:10:33 -0500

    frontend: complete create listing page UI

--- mern-estate/client/src/pages/Profile.jsx
+++ mern-estate/client/src/pages/Profile.jsx
@@ -18,6 +18,7 @@ import {
   signOutUserSuccess,
   signOutUserFailure,
 } from "../redux/user/userSlice";
+import { Link } from "react-router-dom";
 
 // firebase Storage
 // service firebase.storage {
@@ -193,6 +194,9 @@ export default function Profile() {
         >
           {loading ? "loading..." : "update"}
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
