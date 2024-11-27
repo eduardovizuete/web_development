041bb2bedbeaec156f8c30a09d849d5d50ff8838
eduvizuete <vizuete.eduardo@gmail.com>
2024-11-27 10:25:21 -0500

    fix: update express static client dist directry

--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -35,7 +35,7 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));  
